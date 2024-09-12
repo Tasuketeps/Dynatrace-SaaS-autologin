a7c7c98dd46fb6b96ebe7b49319a9c6734cf6c9d
Tasukete <75771070+Tasuketeps@users.noreply.github.com>
2024-09-12 08:47:23 +0800

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,4 +1,4 @@
 @echo off
-cd C:\Users\CS-EDISON.TAY\Desktop\scripts  # Navigate to your script directory
+cd C:path/to/script/directory  # Navigate to your script directory
 python autologin.py  # Run the Python script
 exit
